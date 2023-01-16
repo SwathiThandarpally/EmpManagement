de74b23f142da8aebe59469bc07e4044cb208cfc
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-16 17:38:53 +0530

    Added Emp Wage For Each Company Program

---
+++ EmpManagement/EachCompanyWage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpManagement
+{
+    internal class EachCompanyWage
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EachCompanyWage(string company, int empRatePerHour, int numbOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numbOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+
+        }
+        public void ComputeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+
+                    case 1:
+                    IS_PART_TIME:
+                        empHrs = 16;
+                        break;
+                    case 2:
+                    IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company :" + company + " is " + totalEmpWage);
+            return;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company : " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- EmpManagement/Program.cs
+++ EmpManagement/Program.cs
@@ -9,35 +9,41 @@
             EmployeeCheck.EmployeeAttandance();
             Console.ReadKey();
             Console.WriteLine("-------------------------");
-            //UC_2
-            EmployeeWage employeeWage = new EmployeeWage();
-            employeeWage.FullTimeEmployeeWage();
-            Console.WriteLine("-------------------------");
-            //UC_3
-            AddPartTime addPartTime = new AddPartTime();
-            addPartTime.PartTimeEmpWage();
-            Console.WriteLine("-------------------------");
-            //UC_4
-            CaseStatement caseStatement = new CaseStatement();
-            caseStatement.Display();
-            Console.WriteLine("-------------------------");
-            //UC_5
-            EmpWagePerMonth empWagePerMonth = new EmpWagePerMonth();
-            empWagePerMonth.Display();
-            Console.WriteLine("-------------------------");
-            //UC_6
-            EmpMaxHrs empMaxHrs = new EmpMaxHrs();
-            empMaxHrs.Display();
-            //UC_7
-            RefactorCode calculation = new RefactorCode();
-            calculation.EmployeeAttendence();
-            RefactorCode calculation2 = new RefactorCode();
-            calculation2.EmpWageCalculation();
-            //UC_8
-            MultipleCompanyWage ComputeMultipleCompanies = new MultipleCompanyWage();
-            ComputeMultipleCompanies.ComputeEmpWage("Amazon", 20, 30, 40);
-            ComputeMultipleCompanies.ComputeEmpWage("DMart", 10, 15, 20);
-            ComputeMultipleCompanies.ComputeEmpWage("Vodafone", 5, 15, 20);
+            ////UC_2
+            //EmployeeWage employeeWage = new EmployeeWage();
+            //employeeWage.FullTimeEmployeeWage();
+            //Console.WriteLine("-------------------------");
+            ////UC_3
+            //AddPartTime addPartTime = new AddPartTime();
+            //addPartTime.PartTimeEmpWage();
+            //Console.WriteLine("-------------------------");
+            ////UC_4
+            //CaseStatement caseStatement = new CaseStatement();
+            //caseStatement.Display();
+            //Console.WriteLine("-------------------------");
+            ////UC_5
+            //EmpWagePerMonth empWagePerMonth = new EmpWagePerMonth();
+            //empWagePerMonth.Display();
+            //Console.WriteLine("-------------------------");
+            ////UC_6
+            //EmpMaxHrs empMaxHrs = new EmpMaxHrs();
+            //empMaxHrs.Display();
+            ////UC_7
+            //RefactorCode calculation = new RefactorCode();
+            //calculation.EmployeeAttendence();
+            //RefactorCode calculation2 = new RefactorCode();
+            //calculation2.EmpWageCalculation();
+            ////UC_8
+            //MultipleCompanyWage ComputeMultipleCompanies = new MultipleCompanyWage();
+            //ComputeMultipleCompanies.ComputeEmpWage("Amazon", 20, 30, 40);
+            //ComputeMultipleCompanies.ComputeEmpWage("DMart", 10, 15, 20);
+            //ComputeMultipleCompanies.ComputeEmpWage("Vodafone", 5, 15, 20);
+
+            //UC_9
+            EachCompanyWage eachCompanyWage = new EachCompanyWage("Samsung", 50, 20,100);
+            eachCompanyWage.ComputeEmpWage();
+            eachCompanyWage.ToString();
+            
         }
     }
     
